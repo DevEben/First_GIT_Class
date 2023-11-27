cebe9a6abe088c25b8d5051214f963f81abc99af
DevEben <ebenezertope4@gmail.com>
2023-11-27 17:37:45 +0100

    Set up Server

---
+++ server.js
@@ -0,0 +1,16 @@
+require('./config/config')
+const express = require("express");
+require('dotenv').config();
+const studentRouter = require('./router/routers')
+const port = process.env.port 
+
+const app =  express();
+
+app.use(express.json());
+app.use('/api/student', studentRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+}) 
